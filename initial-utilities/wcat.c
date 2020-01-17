0ccdc0c992227ad86ffc212e1170fc819a4c2f28
tuh24244 <tuh24244@temple.edu>
2020-01-16 21:22:46 -0500

    wcat.c updates, can read from command line

--- initial-utilities/wcat.c
+++ initial-utilities/wcat.c
@@ -1,5 +1,27 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-int main() {
-
+int main(int argc, char* argv[]) {
+	char buffer[50];
+	int lineNum = 0;
+	char charPtr;
+	FILE *fp = fopen(argv[1], "r");
+	if (fp == NULL) {
+		printf("cannot open file\n");
+		exit(1);
+	}
+	charPtr = getc(fp);
+	while (charPtr != EOF) {
+		charPtr = getc(fp);
+		if (charPtr == '\n') {
+			lineNum++;
+		}
+	}
+	rewind(fp);
+	//printf("Number of lines: %d\n", lineNum);
+	for (int i = 0; i < lineNum; i++) {
+		fgets(buffer, 50, fp);
+		printf("%s", buffer);
+	}
+	fclose(fp);
 }
